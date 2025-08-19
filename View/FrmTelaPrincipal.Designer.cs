88458eff92e7705b2bcb65dfd0d14c4e858dea4b
DerJohnBR <derjohnbr@gmail.com>
2025-08-19 20:33:10 -0300

    Adicionar arquivos de projeto.

---
+++ View/FrmTelaPrincipal.Designer.cs
@@ -0,0 +1,147 @@
+﻿namespace SistemaAtendimento
+{
+    partial class FrmTelaPrincipal
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            mnuTelaPrincipal = new MenuStrip();
+            cadastrosToolStripMenuItem = new ToolStripMenuItem();
+            clientesToolStripMenuItem = new ToolStripMenuItem();
+            usuáriosToolStripMenuItem = new ToolStripMenuItem();
+            etapasToolStripMenuItem = new ToolStripMenuItem();
+            statusAtendimentosToolStripMenuItem = new ToolStripMenuItem();
+            atendimentosToolStripMenuItem = new ToolStripMenuItem();
+            novoAtendimentoToolStripMenuItem = new ToolStripMenuItem();
+            consultasToolStripMenuItem = new ToolStripMenuItem();
+            sistemaToolStripMenuItem = new ToolStripMenuItem();
+            sairToolStripMenuItem = new ToolStripMenuItem();
+            mnuTelaPrincipal.SuspendLayout();
+            SuspendLayout();
+            // 
+            // mnuTelaPrincipal
+            // 
+            mnuTelaPrincipal.Items.AddRange(new ToolStripItem[] { cadastrosToolStripMenuItem, atendimentosToolStripMenuItem, sistemaToolStripMenuItem });
+            mnuTelaPrincipal.Location = new Point(0, 0);
+            mnuTelaPrincipal.Name = "mnuTelaPrincipal";
+            mnuTelaPrincipal.Size = new Size(800, 24);
+            mnuTelaPrincipal.TabIndex = 0;
+            mnuTelaPrincipal.Text = "menuStrip1";
+            // 
+            // cadastrosToolStripMenuItem
+            // 
+            cadastrosToolStripMenuItem.DropDownItems.AddRange(new ToolStripItem[] { clientesToolStripMenuItem, usuáriosToolStripMenuItem, etapasToolStripMenuItem, statusAtendimentosToolStripMenuItem });
+            cadastrosToolStripMenuItem.Name = "cadastrosToolStripMenuItem";
+            cadastrosToolStripMenuItem.Size = new Size(71, 20);
+            cadastrosToolStripMenuItem.Text = "Cadastros";
+            // 
+            // clientesToolStripMenuItem
+            // 
+            clientesToolStripMenuItem.Name = "clientesToolStripMenuItem";
+            clientesToolStripMenuItem.Size = new Size(184, 22);
+            clientesToolStripMenuItem.Text = "Clientes";
+            // 
+            // usuáriosToolStripMenuItem
+            // 
+            usuáriosToolStripMenuItem.Name = "usuáriosToolStripMenuItem";
+            usuáriosToolStripMenuItem.Size = new Size(184, 22);
+            usuáriosToolStripMenuItem.Text = "Usuários";
+            // 
+            // etapasToolStripMenuItem
+            // 
+            etapasToolStripMenuItem.Name = "etapasToolStripMenuItem";
+            etapasToolStripMenuItem.Size = new Size(184, 22);
+            etapasToolStripMenuItem.Text = "Etapas";
+            // 
+            // statusAtendimentosToolStripMenuItem
+            // 
+            statusAtendimentosToolStripMenuItem.Name = "statusAtendimentosToolStripMenuItem";
+            statusAtendimentosToolStripMenuItem.Size = new Size(184, 22);
+            statusAtendimentosToolStripMenuItem.Text = "Status Atendimentos";
+            // 
+            // atendimentosToolStripMenuItem
+            // 
+            atendimentosToolStripMenuItem.DropDownItems.AddRange(new ToolStripItem[] { novoAtendimentoToolStripMenuItem, consultasToolStripMenuItem });
+            atendimentosToolStripMenuItem.Name = "atendimentosToolStripMenuItem";
+            atendimentosToolStripMenuItem.Size = new Size(94, 20);
+            atendimentosToolStripMenuItem.Text = "Atendimentos";
+            // 
+            // novoAtendimentoToolStripMenuItem
+            // 
+            novoAtendimentoToolStripMenuItem.Name = "novoAtendimentoToolStripMenuItem";
+            novoAtendimentoToolStripMenuItem.Size = new Size(176, 22);
+            novoAtendimentoToolStripMenuItem.Text = "Novo Atendimento";
+            // 
+            // consultasToolStripMenuItem
+            // 
+            consultasToolStripMenuItem.Name = "consultasToolStripMenuItem";
+            consultasToolStripMenuItem.Size = new Size(176, 22);
+            consultasToolStripMenuItem.Text = "Consultas";
+            // 
+            // sistemaToolStripMenuItem
+            // 
+            sistemaToolStripMenuItem.DropDownItems.AddRange(new ToolStripItem[] { sairToolStripMenuItem });
+            sistemaToolStripMenuItem.Name = "sistemaToolStripMenuItem";
+            sistemaToolStripMenuItem.Size = new Size(60, 20);
+            sistemaToolStripMenuItem.Text = "Sistema";
+            // 
+            // sairToolStripMenuItem
+            // 
+            sairToolStripMenuItem.Name = "sairToolStripMenuItem";
+            sairToolStripMenuItem.Size = new Size(93, 22);
+            sairToolStripMenuItem.Text = "Sair";
+            // 
+            // FrmTelaPrincipal
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(mnuTelaPrincipal);
+            MainMenuStrip = mnuTelaPrincipal;
+            Name = "FrmTelaPrincipal";
+            Text = "Sistema de Atendimento";
+            Load += FrmTelaPrincipal_Load;
+            mnuTelaPrincipal.ResumeLayout(false);
+            mnuTelaPrincipal.PerformLayout();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private MenuStrip mnuTelaPrincipal;
+        private ToolStripMenuItem cadastrosToolStripMenuItem;
+        private ToolStripMenuItem clientesToolStripMenuItem;
+        private ToolStripMenuItem usuáriosToolStripMenuItem;
+        private ToolStripMenuItem etapasToolStripMenuItem;
+        private ToolStripMenuItem statusAtendimentosToolStripMenuItem;
+        private ToolStripMenuItem atendimentosToolStripMenuItem;
+        private ToolStripMenuItem novoAtendimentoToolStripMenuItem;
+        private ToolStripMenuItem consultasToolStripMenuItem;
+        private ToolStripMenuItem sistemaToolStripMenuItem;
+        private ToolStripMenuItem sairToolStripMenuItem;
+    }
+}
---
+++ View/FrmTelaPrincipal.cs
@@ -0,0 +1,15 @@
+namespace SistemaAtendimento
+{
+    public partial class FrmTelaPrincipal : Form
+    {
+        public FrmTelaPrincipal()
+        {
+            InitializeComponent();
+        }
+
+        private void FrmTelaPrincipal_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
